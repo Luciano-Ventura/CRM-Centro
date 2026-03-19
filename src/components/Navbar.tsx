4771b2b948ae3727ff9c305db3107b5659cfdcc6
Luciano-Ventura <jrsouzahh@gmail.com>
2026-03-19 20:41:29 -0300

    Fix: mobile-first correçãoV3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client'
 
 import { useState } from 'react'
 import Link from 'next/link'
 import styles from './Navbar.module.css'
+/* eslint-disable @next/next/no-img-element */
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
@@ -11,10 +12,10 @@ export default function Navbar() {
     <nav className={styles.navbar}>
       <div className={styles.container}>
         <Link href="/" className={styles.logo}>
-          Vovô Congo
+          <img src="/images/logo.png" alt="Logo CUVCCA" className={styles.logoImage} onError={(e) => { e.currentTarget.style.display = 'none'; }} />
+          <span>C.U.V.C.C.A</span>
         </Link>
         
-        {/* Hamburger Icon */}
         <button className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
           <svg width="28" height="28" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
             {isOpen ? (
@@ -25,14 +26,16 @@ export default function Navbar() {
           </svg>
         </button>
 
-        <div className={`${styles.navLinks} ${isOpen ? styles.navLinksOpen : ''}`}>
-          <Link href="#inicio" className={styles.link} onClick={() => setIsOpen(false)}>Início</Link>
-          <Link href="#sobre" className={styles.link} onClick={() => setIsOpen(false)}>Sobre Nós</Link>
-          <Link href="#giras" className={styles.link} onClick={() => setIsOpen(false)}>Giras</Link>
-          <Link href="#fazer-parte" className={styles.link} onClick={() => setIsOpen(false)}>Faça Parte</Link>
-          <Link href="#contato" className={styles.link} onClick={() => setIsOpen(false)}>Contato</Link>
-          
-          <div className={styles.authButtons}>
+        <div className={`${styles.navContainer} ${isOpen ? styles.navContainerOpen : ''}`}>
+          <ul className={styles.navLinks}>
+            <li><Link href="#inicio" className={styles.navLink} onClick={() => setIsOpen(false)}>Início</Link></li>
+            <li><Link href="#sobre" className={styles.navLink} onClick={() => setIsOpen(false)}>Sobre Nós</Link></li>
+            <li><Link href="#giras" className={styles.navLink} onClick={() => setIsOpen(false)}>Giras</Link></li>
+            <li><Link href="#faca-parte" className={styles.navLink} onClick={() => setIsOpen(false)}>Faça Parte</Link></li>
+            <li><Link href="#contato" className={styles.navLink} onClick={() => setIsOpen(false)}>Contato</Link></li>
+          </ul>
+
+          <div className={styles.actions}>
             <Link href="/login" className={styles.btnOutline} onClick={() => setIsOpen(false)}>Entrar</Link>
             <Link href="/register" className={styles.btnPrimary} onClick={() => setIsOpen(false)}>Cadastrar</Link>
           </div>
